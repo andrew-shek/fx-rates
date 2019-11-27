17a51b0ad55808600fac04a38b07c58426ee32a7
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-27 16:18:54 +1100

    initial commit

---
+++ src/main/kotlin/com/airwallex/growth/fxrates/FxRatesApplication.kt
@@ -0,0 +1,11 @@
+package com.airwallex.growth.fxrates
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FxRatesApplication
+
+fun main(args: Array<String>) {
+	runApplication<FxRatesApplication>(*args)
+}
---
+++ src/test/kotlin/com/airwallex/growth/fxrates/FxRatesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.airwallex.growth.fxrates
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FxRatesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
