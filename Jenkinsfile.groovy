7143f50557072915a57ef37ac5cc9aa16bca3cdc
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-28 16:52:42 +1100

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
                     g_rls_img_tag = env.BRANCH_NAME.replaceAll(~/\//, "-").toLowerCase()
                     //sh "docker build --tag asia.gcr.io/shektest/Testing-fx-rates:feature-testing1 ."
                     //sh "docker push asia.gcr.io/shektest/Testing-fx-rates:feature-testing1"
-                    sh "${WORKSPACE}/scripts/docker-build-upload.sh Testing-fx-rates ${g_rls_img_tag} ${env.BRANCH_NAME}"
+                    sh "${WORKSPACE}/scripts/docker-build-upload.sh testing-fx-rates ${g_rls_img_tag} ${env.BRANCH_NAME}"
                 }
             }
         }
