f7faf093a1b2f4c7a0228f1f0ed8035a6de2fb77
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-28 16:50:48 +1100

    installed docker on jenkins host

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,9 +17,9 @@ pipeline {
                 script {
                     echo 'Build and publish docker image'
                     g_rls_img_tag = env.BRANCH_NAME.replaceAll(~/\//, "-").toLowerCase()
-                    sh "docker build --tag asia.gcr.io/shektest/Testing-fx-rates:feature-testing1 ."
-                    sh "docker push asia.gcr.io/shektest/Testing-fx-rates:feature-testing1"
-                    //sh "${WORKSPACE}/scripts/docker-build-upload.sh Testing-fx-rates ${g_rls_img_tag} ${env.BRANCH_NAME}"
+                    //sh "docker build --tag asia.gcr.io/shektest/Testing-fx-rates:feature-testing1 ."
+                    //sh "docker push asia.gcr.io/shektest/Testing-fx-rates:feature-testing1"
+                    sh "${WORKSPACE}/scripts/docker-build-upload.sh Testing-fx-rates ${g_rls_img_tag} ${env.BRANCH_NAME}"
                 }
             }
         }
