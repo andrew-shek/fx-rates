406438b96483d1b5569ab97ad4f656e6a97b6c81
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-29 13:55:12 +1100

    add gcp project

--- scripts/docker-build-upload.sh
+++ scripts/docker-build-upload.sh
@@ -7,7 +7,7 @@ REGISTRY=asia.gcr.io
 
 RELEASE_URI="$REGISTRY/${IMG_REPO}/$APP_NAME"
 RELEASE_FULL_URI="${RELEASE_URI}:${IMG_TAG}"
-
+GCP_PROJEXT='growth-dev-landing'
 
 echo "REGISTRY: $REGISTRY"
 echo "APP_NAME: $APP_NAME"
@@ -17,12 +17,13 @@ echo "RELEASE_FULL_URI: ${RELEASE_FULL_URI}"
 echo "BRANCH: ${BRANCH}"
 
 
-docker build --tag "${RELEASE_FULL_URI}" .
+docker build --tag "${IMG_TAG}" .
 
 
 # Generate a Release ID json file
 
 
 IMAGE_TAG="$REGISTRY/${IMG_REPO}/$APP_NAME:$RELEASE_FULL_URI"
+IMAGE_TAG="$REGISTRY/${GCP_PROJEXT}/$APP_NAME:$IMG_TAG"
 echo "Pushing ${IMAGE_TAG} to registry..."
 docker push "${RELEASE_FULL_URI}"
