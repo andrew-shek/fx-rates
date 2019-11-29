9f81e7d186032f4d9362c9b5e0ad477ba6952486
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-29 12:10:35 +1100

    test

--- scripts/docker-build-upload.sh
+++ scripts/docker-build-upload.sh
@@ -14,14 +14,16 @@ echo "APP_NAME: $APP_NAME"
 echo "RELEASE_URI: ${RELEASE_URI}"
 echo "IMG_TAG: ${IMG_TAG}"
 echo "RELEASE_FULL_URI: ${RELEASE_FULL_URI}"
+echo "BRANCH: ${BRANCH}"
 
 
 docker build --tag "${RELEASE_FULL_URI}" .
-# BUILD_SHA=$(docker push "${RELEASE_FULL_URI}" | awk '/digest: sha256/ {print $3}')
+
 
 # Generate a Release ID json file
-# echo "BUILD SHA: ${BUILD_SHA}"
-# printf "{'repo':'%s', 'app_name':'%s', 'build_sha':'%s', 'img_tag':'%s', 'release_uri':'%s'}" "${IMG_REPO}" "${APP_NAME}" "$BUILD_SHA" "$IMG_TAG" "$RELEASE_URI" >> rls_container.json
+
+
 
 IMAGE_TAG="$REGISTRY/${IMG_REPO}/$APP_NAME:$BRANCH"
+echo "Pushing ${IMAGE_TAG} to registry..."
 docker push "$IMAGE_TAG"
