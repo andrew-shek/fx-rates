09322348773ada707672a8c1cbe0b90aee82a4fe
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-11-29 12:03:38 +1100

    remove build_sha

--- scripts/docker-build-upload.sh
+++ scripts/docker-build-upload.sh
@@ -17,11 +17,11 @@ echo "RELEASE_FULL_URI: ${RELEASE_FULL_URI}"
 
 
 docker build --tag "${RELEASE_FULL_URI}" .
-BUILD_SHA=$(docker push "${RELEASE_FULL_URI}" | awk '/digest: sha256/ {print $3}')
+# BUILD_SHA=$(docker push "${RELEASE_FULL_URI}" | awk '/digest: sha256/ {print $3}')
 
 # Generate a Release ID json file
-echo "BUILD SHA: ${BUILD_SHA}"
-printf "{'repo':'%s', 'app_name':'%s', 'build_sha':'%s', 'img_tag':'%s', 'release_uri':'%s'}" "${IMG_REPO}" "${APP_NAME}" "$BUILD_SHA" "$IMG_TAG" "$RELEASE_URI" >> rls_container.json
+# echo "BUILD SHA: ${BUILD_SHA}"
+# printf "{'repo':'%s', 'app_name':'%s', 'build_sha':'%s', 'img_tag':'%s', 'release_uri':'%s'}" "${IMG_REPO}" "${APP_NAME}" "$BUILD_SHA" "$IMG_TAG" "$RELEASE_URI" >> rls_container.json
 
 IMAGE_TAG="$REGISTRY/${IMG_REPO}/$APP_NAME:$BRANCH"
 docker push "$IMAGE_TAG"
