e92ef321eb51c97708b432415c2d2d30bda3a06d
andrew-shek <56807349+andrew-shek@users.noreply.github.com>
2019-12-02 16:15:24 +1100

    add gcloud commands

--- scripts/docker-build-upload.sh
+++ scripts/docker-build-upload.sh
@@ -19,6 +19,15 @@ echo "BRANCH: ${BRANCH}"
 BUILD_IMAGE="$REGISTRY/${GCP_PROJEXT}/$APP_NAME:$IMG_TAG"
 docker build --tag "${BUILD_IMAGE}" .
 
+# install gcloud
+echo "install gcloud"
+pwd
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+gcloud init
+
 
 # Generate a Release ID json file
 echo "docker login"
